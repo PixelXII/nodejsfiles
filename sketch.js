458a276d7e6c694f25beb2d6ee3c1ef59010d483
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-14 09:05:35 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,18 @@
-var x, y, speedX, speedY;
-var diam = 10;
-var rectSize = 200;
+var x = 150, y = 150;
 
 function setup() {
-  createCanvas(1200, 700)
-  fill(0, 255, 0);
-  reset();
+  createCanvas(500, 500)
 }
 
-function reset() {
-  x = width/2;
-  y = height/2;
-  speedX = random(3, 5);
-  speedY = random(3, 5);
-}
-
-function draw() { 
-  background(0);
-  
-  ellipse(x, y, diam, diam);
-
-  rect(0, 0, 20, height);
-  rect(width-30, mouseY-rectSize/2, 10, rectSize);
-
-  x += speedX;
-  y += speedY;
-
-  // if ball hits movable bar, invert X direction
-  if ( x > width-30 && x < width -20 && y > mouseY-rectSize/2 && y < mouseY+rectSize/2 ) {
-    speedX = speedX * -1;
-  } 
-
-  // if ball hits wall, change direction of X
-  if (x < 25) {
-    speedX *= -1.1;
-    speedY *= 1.1;
-    x += speedX;
+function draw() {
+  background(80, 165, 131)
+  fill(255, 87, 51)
+  ellipse(x, y, 15, 15)
+  if(x > 500) {
+    x--
+  } else if(y > 498) {
+    y--
+  } else {
+    x++
   }
-
-
-  // if ball hits up or down, change direction of Y   
-  if ( y > height || y < 0 ) {
-    speedY *= -1;
-  }
-}
-
-function mousePressed() {
-  reset();
 }
