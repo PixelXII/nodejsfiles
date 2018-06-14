fbb3bd6105b01d64d40cc2295390614d10358ec7
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-14 09:06:40 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,9 @@ function draw() {
   background(80, 165, 131)
   fill(255, 87, 51)
   ellipse(x, y, 15, 15)
-  if(x > 500) {
+  if(x == 500) {
     x--
-  } else if(y > 498) {
+  } else if(y == 500) {
     y--
   } else {
     x++
