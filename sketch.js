393542e3f2172cbbbebab037cb6c0173f40c9db2
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-13 21:41:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
-function setup() {}
+function setup() {
+  createCanvas(640, 480)
+}
 
 function draw() {
   ellipse(50, 50, 80, 80)
- }
+  if (mouseIsPressed) {
+    fill(0)
+    stroke(24, 53, 150)
+  } else {
+    fill(40)
+  }
+}
