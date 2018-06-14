c101678a2b9a56dfbf363e354dd350514ae8f756
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-14 09:31:07 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,15 +7,15 @@ function setup() {
 function draw() {
   background(80, 165, 131)
   stroke(255, 87, 51)
-  line(mouseX, mouseY, mouseX, mouseY)
-  line(mouseY, mouseY, 314, mouseY)
-  line(mouseX, mouseX, mouseY, mouseY)
-  line(108, mouseX, mouseY, mouseY)
-  line(mouseY, mouseY, mouseY, mouseY)
-  line(mouseX, mouseX, mouseX, mouseY)
-  line(mouseY, mouseY, mouseY, mouseY)
-  line(mouseY, 470, mouseX, mouseY)
-  line(mouseX, mouseX, 230, mouseY)
+  line(mouseX, mouseY, mouseX+12, mouseY+12)
+  line(mouseY, mouseY, 314, mouseY+12)
+  line(mouseX, mouseX, mouseY, mouseY+12)
+  line(108, mouseX, mouseY, mouseY+12)
+  line(mouseY, mouseY, mouseY, mouseY+12)
+  line(mouseX, mouseX, mouseX, mouseY+12)
+  line(mouseY, mouseY, mouseY, mouseY+12)
+  line(mouseY, 470, mouseX, mouseY+12)
+  line(mouseX, mouseX, 230, mouseY+12)
   x+=4
   y+=5
 }
