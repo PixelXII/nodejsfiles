179753fd870f90a1892512d2ec141c86678e22b5
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-31 19:47:01 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,9 @@
 var x = 150, y = 150;
 
 function setup() {
-  createCanvas(500, 500)
+  createCanvas(800, 800)
 }
 
 function draw() {
-  background(80, 165, 131)
-  fill(80, 165, 131)
-  stroke(255, 87, 51)
-  line(mouseX, mouseY, mouseX+12, mouseY+12)
-  line(mouseY, mouseY, 314, mouseY+12)
-  line(mouseX, mouseX, mouseY, mouseY+12)
-  line(mouseY, mouseX, mouseY, mouseY+12)
-  rect(180, mouseY, 15, 16)
-  line(mouseY, mouseY, mouseY, mouseY+12)
-  line(mouseX, mouseX, mouseX, mouseY+12)
-  line(mouseY, mouseY, mouseY, mouseY+12)
-  line(mouseY, 470, mouseX, mouseY+12)
-  line(mouseX, mouseX, 230, mouseY+12)
-  x+=4
-  y+=5
+ 
 }
