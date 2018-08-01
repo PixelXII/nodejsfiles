97bcef65b1642a9e22549e5c640df63901c497ad
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-31 20:00:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,3 +7,27 @@ function setup() {
 function draw() {
  
 }
+
+// BUBBLES
+
+var x = random(0, 400)
+var y = random(0, 400)
+function Bubble(w, h, r) {
+  this.w = w
+  this.h = h
+  this.x = x
+  this.y = y
+  this.r = r
+}
+
+Bubble.prototype.draw = function() {
+  stroke(255)
+  strokeWeight(4)
+  ellipse(this.x, this.y, this.w, this.h, this.r)
+}
+
+Bubble.prototype.jiggle = function() {
+  this.x -= random(0, 5)
+  this.y += random(0, 3)
+}
+
